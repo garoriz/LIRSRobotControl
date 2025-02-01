e7110d46dbc154431d69ea3826020d317013a63e
rizvan <rizvangaripov@gmail.com>
2025-02-01 11:55:51 +0300

    Add robot-selection-module

--- robot-selection-feature/build.gradle.kts
+++ robot-selection-feature/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "com.garif.lirsrobotcontrol"
-    compileSdk = 35
+    namespace = "com.garif.robot_contreol_feature"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.garif.lirsrobotcontrol"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,12 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(project(":robot-selection-feature"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "LIRSRobotControl"
 include(":app")
+include(":robot-selection-feature")
